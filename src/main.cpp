e672117f46903df2b0fbf5dee6cae703c80739f8
Jason Vlajankov <jvlaj@protonmail.com>
2023-12-01 17:25:29 +1100

    dielectrics

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,15 @@ int main() {
 
     // Materials
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_centre = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_centre = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     // Objects
     world.add(make_shared<sphere>(point3(0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_centre));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     camera cam;
